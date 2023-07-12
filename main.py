e7b0f4025472ba61e2b2be66a9bee9a4b55a195e
EntropyNetwork <117935093+EntropyNetwork@users.noreply.github.com>
2023-07-12 10:15:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import regex
+
 def parseFirstMove(firstMove):
 	if regexDrinkHemlock(firstMove):
 		print("You chose to believe the GUARD, DRINK HEMLOCK, and died. Your score was 100.")
 	else:
-		print("You appear to have done something other than choosing to DRINK HEMLOCK. This appears to have worked! Your score is i. Type HELP for options.")
+		print("You chose to do something other than DRINK HEMLOCK.")
+		print("The GUARD appears upset and shakes his head. 'Fine, then, Philosopher. Persist in this doomed Athens without the favor of the gods.'")
+		print("It appears to have worked! Your score is i. Type ACTIONS to pull up the ACTIONS menu.")
 
 def main():
 	print("Some members of the GUARD are here and waiting for you to DRINK HEMLOCK.")
